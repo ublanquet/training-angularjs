c3501e1cc8f62dbb9fca110fde5507314b5e8c51
UGO <ublanquet@e-biz.fr>
2017-05-31 17:21:34 +0200

    inject css with gulp for build

--- gulp/tasks/styles.tasks.js
+++ gulp/tasks/styles.tasks.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
 const fs = require('fs-extra');
-
+var inject = require('gulp-inject');
 const conf = require('../../conf/gulp.conf');
 const plumber = require('gulp-plumber');
 
@@ -28,8 +28,9 @@ function injectCss(cb) {
         fs.copySync(srcIndex, dstIndex);
     }
 
-    // TODO use gulp-inject to wire up css in index.html
+    var cssInject = gulp.src('./src/**/*.css', {read: false});
     return gulp.src(dstIndex, {base: conf.path.tmp()})
+        .pipe(inject(cssInject))
         .pipe(gulp.dest(conf.paths.tmp))
         .on('end', cb);
 }
